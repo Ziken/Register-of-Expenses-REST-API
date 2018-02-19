4e5be413a9534a7d40aefcdb7f2410ea5282e5ea
Ziken <patrykkazmierczak97@gmail.com>
2018-02-19 21:56:16 +0100

    Add POST /expenses route

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const mongoose = require('./db/mongoose');
+const {Expense} = require('./models/expense');
+
+const app = express();
+
+app.use(require('body-parser').json());
+
+app.post('/expenses', (req, res) => {
+    const {title, amount, spentAt} = req.body;
+    const expense = new Expense({title, amount, spentAt});
+
+    expense.save().then((result) => {
+        res.send({result});
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Application is running on port 3000');
+});
